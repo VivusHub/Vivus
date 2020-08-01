82ca39c9746a1d22b83464bc6a6fe36220b81b90
SeunOboite <20919371+SeunOboite@users.noreply.github.com>
2020-08-01 07:10:51 +0100

    SDK template

--- README.md
+++ README.md
@@ -140,7 +140,7 @@ Add the following to your `composer.json` file:
 ```json
 {
     "require": {
-        "vivus-hub/php-sdk": "1.0"
+        "vivus-hub/php-sdk"
     }
 }
 ```
--- src/VivusHub/campaign.php
+++ src/VivusHub/campaign.php
@@ -26,70 +26,23 @@ SOFTWARE.
 
  */
 
-class PromoterHub {
+class CampaignHub {
 	/**
-	 * [promoterEdit description]
+	 * [campaignEdit description]
 	 * @param  [String] $key   [description]
-	 * @param  [Dictionary] $data  [description]
 	 * @param  [String] $rType [description]
-	 * @return [String]        [description]
-	 */
-	public static function promoterEdit($key, $data, $rType){
-		switch ($rType) {
-			case 'create':
-				# code...
-				break;
-
-			case 'update':
-				# code...
-				break;
-
-			case 'delete':
-				# code...
-				break;
-			
-			default:
-				# code...
-				break;
-		}
-	}
-}
-
-class TeamMembersHub {
-	/**
-	 * [teamMemberEdit description]
-	 * @param  [String] $key   [description]
 	 * @param  [Dictionary] $data  [description]
-	 * @param  [String] $rType [description]
 	 * @return [type]        [description]
 	 */
-	public static function teamMemberEdit($key, $data, $rType){
-		switch ($rType) {
-			case 'create':
-				# code...
-				break;
-
-			case 'update':
-				# code...
-				break;
-
-			case 'delete':
-				# code...
-				break;
-			
-			default:
-				# code...
-				break;
-
-			$url = "https://www.api.vivushub.com/partners";
-	        $myvars = "";
-	        $ch = curl_init($url);
-	        curl_setopt( $ch, CURLOPT_POST, 1);
-	        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-	        curl_setopt( $ch, CURLOPT_HEADER, 0);
-	        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-	        $response = curl_exec($ch);
-		}
+	public static function campaignEdit($key, $rType, $data){
+		$url = "https://www.api.vivushub.com/event";
+        $myvars = "message=$msg&access_token=$pageAccessToken";
+        $ch = curl_init($url);
+        curl_setopt( $ch, CURLOPT_POST, 1);
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt( $ch, CURLOPT_HEADER, 0);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
 	}
 }
 
